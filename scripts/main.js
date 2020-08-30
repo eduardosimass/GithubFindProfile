c917b55b1f93ad92ef28d891f7b0370746010c8b
eduardosimass <simaseduardo2@gmail.com>
2020-08-30 16:05:52 -0300

    commit
    Signed-off-by: eduardosimass <simaseduardo2@gmail.com>

---
+++ scripts/main.js
@@ -0,0 +1,98 @@
+/* constantes e closure para negar acesso */
+
+(function () {
+  const search = document.querySelector("#search");
+  const profile = document.querySelector("#profile");
+  const url = "https://api.github.com/users";
+  const client_id = "Iv1.8bfd24d9acf14878";
+  const client_secret = "42f28ee69d2bed574ebe05fe84188393d5ea0283";
+  const count = "per_page=3";
+
+
+  /* pegando valor do search*/
+  async function getUser(user) {
+    const profileResponse = await fetch(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+
+    const reposResponse = await fetch(`${url}/${user}/repos?${count}&client_id=${client_id}&client_secret=${client_secret}`);
+
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return { profile, repos };
+  }
+
+
+
+
+  function showDataProfile(user) {
+
+
+    profile.innerHTML =
+      `<div class="row" mt-3>
+    <div class="card" style="width: 18rem">
+      <img class="card-img-top" src="${user.avatar_url}">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Nome : <span class="badge badge-success">${user.name}</span></li>
+        <li class="list-group-item">Bio:<span class="badge badge-success">${user.bio}</span></li>
+        <li class="list-group-item">Email:<span class="badge badge-success">${user.email}</span></li>
+        <li class="list-group-item">Repositórios : <span class="badge badge-success">${user.public_repos}</span></li>
+        <li class="list-group-item">Seguidores : <span class="badge badge-primary">${user.followers}</span></li>
+        <li class="list-group-item">Seguindo: <span class="badge badge-info">${user.following}</span></li>
+      </ul>
+      <div class="card-body">
+       
+        <a href="${user.html_url}" target="_blank" class="btn btn-warning btn-block">Ver perfi</a>
+      </div>
+    </div>
+    <div class="row  ml-3 ">
+      <div id="repos"></div>
+    </div>
+  </div>`
+  }
+
+
+  function showRepos(repos) {
+    let output = "";
+    repos.forEach(repo => {
+      output += `
+            
+            <div class="card card-body mb-2 ml-3>
+            <div class="row" ">
+            <div class="row col-md-6">Repositório:<p class="center">${repo.name}</p>
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count} </span>
+              <span class="badge badge-success">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-warning">Forks: ${repo.stargazers_count}</span>
+      </div>               
+      <div class="card-body">
+       
+        <a href="${repo.html_url}" target="_blank" class="btn btn-warning btn-block">Ver detalhes do Repositório</a>
+      </div>
+              </div>
+            </div>
+        
+          </div></div>`
+
+      document.getElementById('repos').innerHTML = output;
+
+    });
+
+  }
+
+
+
+  search.addEventListener('keyup', (e) => {
+    const user = e.target.value;
+
+    if (user.length > 0) {
+      getUser(user).then(res => {
+        showDataProfile(res.profile)
+        showRepos(res.repos)
+      });
+    }
+
+  })
+})();
+
